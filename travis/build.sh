bc828015db433dd1139275e0d185de02cf3d8249
Reto Schneider <code@reto-schneider.ch>
2016-02-21 00:09:51 +0100

    Transform the Travis CI build script into an easier to read form

---
+++ travis/build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e #Exit on first error
+
+#Check out the source code
+git clone https://github.com/llvm-mirror/llvm.git --depth 1
+git clone https://github.com/llvm-mirror/clang.git --depth 1 llvm/tools/clang
+git clone https://github.com/llvm-mirror/clang-tools-extra.git --depth 1 \
+ llvm/tools/clang/tools/extra
+git clone --branch=${TRAVIS_BRANCH} \
+ https://github.com/rettichschnidi/clang-tidy-misra.git --depth 1 \
+ llvm/tools/clang/tools/extra/clang-tidy/misra/
+
+#Patch clang-tidy to pick up our code
+(cd llvm/tools/clang/tools/extra/ &&
+ patch -p1 < clang-tidy/misra/clang-tidy.patch)
+
+#Configure, build and test
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ../llvm
+make VERBOSE=1 -j4 check-tidy-misra
