d8be3a69278ba0157cb3a8ce0c1730098da9d048
Reto Schneider <code@reto-schneider.ch>
2016-03-07 02:56:56 +0100

    Cosmetics: Fix formatting

--- test/c2012/1.2.c
+++ test/c2012/1.2.c
@@ -1,3 +1,3 @@
 // RUN: %clang -std=c99 -pedantic-errors -fsyntax-only -ferror-limit=0 -Xclang -verify %s
 
-int i = 0b101010;  // expected-error {{binary integer literals are a GNU extension}}
+int i = 0b101010; // expected-error {{binary integer literals are a GNU extension}}
