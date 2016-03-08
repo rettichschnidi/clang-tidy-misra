a9a8203dbd41d13265efeb7d592571581180a933
Reto Schneider <code@reto-schneider.ch>
2016-03-08 23:43:23 +0100

    Add test for MISRA C 2012 rule 5.4 to wishlist directory

---
+++ test-wishlist/c2012/5.4.c
@@ -0,0 +1,7 @@
+// .....0123456789012345678901234567890!!!
+#define engine_exhaust_gas_temperature_raw egt_r
+#define engine_exhaust_gas_temperature_scaled egt_s // Non-Compliant
+
+// .....012345678901234567890123456
+#define engine_exhaust_gas_temp_raw egt_r
+#define engine_exhaust_gas_temp_scaled egt_s // Compliant
