9200eadd2cde76fefd877c87b8d67f5dc282601e
Reto Schneider <rettichschnidi@users.noreply.github.com>
2016-04-18 13:19:28 +0200

    Fix parameter name

--- test/common/enable-IgnoreCommandLineLocations.cpp
+++ test/common/enable-IgnoreCommandLineLocations.cpp
@@ -1,4 +1,4 @@
-// RUN: %python %S/../check_clang_tidy.py %s misra-cpp2008-17-0-1 %t -config='{CheckOptions: [{key: misra.IgnoreCommandLineLocations, value: 1}]}' -- -D__MY_DANGEROUS_MACRO
+// RUN: %python %S/../check_clang_tidy.py %s misra-cpp2008-17-0-1 %t -config='{CheckOptions: [{key: misra-cpp2008-17-0-1.IgnoreCommandLineLocations, value: 1}]}' -- -D__MY_DANGEROUS_MACRO
 // Do not generate an error for -D__MY_DANGEROUS_MACRO
 
 #define __cplusplus // CHECK-MESSAGES: :[[@LINE]]:9: warning: Reserved identifiers, macros and functions in the standard library shall not be defined, redefined or undefined. [misra-cpp2008-17-0-1]
