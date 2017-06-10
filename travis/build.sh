fb9179aa0d826ac3d23163f148019a346e73a80a
Reto Schneider <code@reto-schneider.ch>
2017-06-10 18:49:39 +0200

    Increase number of parallel compile processes
    Justification: Push the build time below 50min enforced by Travis CI.

--- travis/build.sh
+++ travis/build.sh
@@ -20,4 +20,4 @@ mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=X86 \
  -DBUILD_SHARED_LIBS=ON ../llvm
-make VERBOSE=1 -j4 check-tidy-misra
+make VERBOSE=1 -j5 check-tidy-misra
