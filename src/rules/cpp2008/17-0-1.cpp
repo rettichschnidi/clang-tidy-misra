559dbe6753236742c23acda4d04add0ccae464f2
Reto Schneider <code@reto-schneider.ch>
2017-06-10 17:02:50 +0200

    Adapt to new signature of PPChainedCallbacks::MacroUndefined()

--- src/rules/cpp2008/17-0-1.cpp
+++ src/rules/cpp2008/17-0-1.cpp
@@ -53,7 +53,8 @@ public:
 
   // Detect #undef
   virtual void MacroUndefined(const Token &MacroNameTok,
-                              const MacroDefinition &) override {
+                              const MacroDefinition & /*MD*/,
+                              const MacroDirective * /*Undef*/) override {
     detectViolation(MacroNameTok);
   }
 
